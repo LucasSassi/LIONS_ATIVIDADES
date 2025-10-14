51047b633e480686b7224809386a1b088b78eba0
Lucas Sassi <sassi.lucas17@gmail.com>
2025-10-14 10:59:08 -0300

    Deixando codigos alinhados

--- UNIDADE_3/API/crudFigurinhas/modules/Users/delete.js
+++ UNIDADE_3/API/crudFigurinhas/modules/Users/delete.js
@@ -1,22 +1,18 @@
 import Mfig from "../../models/SchemaFigurinha.js";
 import MUser from "../../models/schemaUsers.js";
 
-export async function deleteUser (req, res){
-    try{
-    const idParaDeletar = req.params.id
-    
-    const deletarUser = await MUser.findByIdAndDelete(idParaDeletar)
-  
-    const figsParaDeletar = await Mfig.deleteMany({UserId: idParaDeletar})
+export async function deleteUser(req, res) {
+  try {
+    const idParaDeletar = req.params.id;
 
-    res.status(200).send(`User deletado com sucesso!`, deletarUser, figsParaDeletar);
+    const deletarUser = await MUser.findByIdAndDelete(idParaDeletar);
 
-    
-    }
-    catch (err){
-      console.error("O erro é: ", err)
-    }
-  };
-  
-  
-  
+    const figsParaDeletar = await Mfig.deleteMany({ UserId: idParaDeletar });
+
+    res
+      .status(200)
+      .send(`User deletado com sucesso!`, deletarUser, figsParaDeletar);
+  } catch (err) {
+    console.error("O erro é: ", err);
+  }
+}
