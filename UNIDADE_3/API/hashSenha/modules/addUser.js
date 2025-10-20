89b36db859972817b49392f7ea85239e3b076acb
Lucas Sassi <sassi.lucas17@gmail.com>
2025-10-20 11:28:08 -0300

    começando a estudar hash de senhas

--- UNIDADE_3/API/hashSenha/modules/addUser.js
+++ UNIDADE_3/API/hashSenha/modules/addUser.js
@@ -1,31 +1,32 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcrypt";
 import MUser from "../models/schemaUser.js";
 
 export async function addUser(req, res) {
-    try {
-        const dados = req.body;
+  try {
+    const dados = req.body;
 
-        const userExists = await MUser.findOne({ email: dados.email });
-        
-        if (userExists) {
-            return res.status(409).json({ message: "Este email já está cadastrado." });
-        }
+    const userExists = await MUser.findOne({ email: dados.email });
 
-        const hashSenha = await bcrypt.hash(dados.senha, 12);
-        
-        const newUser = await MUser.create({
-            ...dados,
-            senha: hashSenha
-        });
-
-        return res.status(201).json(newUser);
-
-    } catch (err) {
-        console.error(`O erro é: ${err}`);
-        
-        return res.status(500).json({ 
-            message: "Não foi possível criar o usuário.",
-            error: err.message
-        });
+    if (userExists) {
+      return res
+        .status(409)
+        .json({ message: "Este email já está cadastrado." });
     }
+
+    const hashSenha = await bcrypt.hash(dados.senha, 12);
+
+    const newUser = await MUser.create({
+      ...dados,
+      senha: hashSenha,
+    });
+
+    return res.status(201).json(newUser);
+  } catch (err) {
+    console.error(`O erro é: ${err}`);
+
+    return res.status(500).json({
+      message: "Não foi possível criar o usuário.",
+      error: err.message,
+    });
+  }
 }
