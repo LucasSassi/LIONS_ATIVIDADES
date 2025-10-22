351e85beed48cd040c6594b982048c4f437ab4c2
Lucas Sassi de Souza <sassi.lucas17@gmail.com>
2025-10-22 15:16:34 -0300

    testando para corrigir erros

--- UNIDADE_3/API/hashSenha/connect.js
+++ UNIDADE_3/API/hashSenha/connect.js
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import { router } from "./routes.js";
 import mongoose from "mongoose";
@@ -7,8 +7,7 @@ import mongoose from "mongoose";
 const app = express();
 const port = 2323;
 
-
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL);
 
 mongoose.connection.once("open", () => {
   console.log("Connected to databank");
@@ -18,12 +17,10 @@ mongoose.connection?.on("error", (err) => {
   console.error(`Something went wrong trying to connect to databank, ${err}`);
 });
 
-app.use(express.json())
-
+app.use(express.json());
 
 app.use(express.json());
 app.use(router);
-
 
 app.listen(port, () => {
   console.log(`Server working: http://localhost:${port}`);
@@ -32,6 +29,6 @@ app.listen(port, () => {
 /*
 
 DATABASE_URL="mongodb+srv://lions123:lions123@cluster0.kt8yoxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-token="36c5e560ecb4d0cac76541eb981301959079c85277db27f5da9e1357102f7468"
+JWT_SECRET="e9c23519647e3abc7695ea0f3c112b203a5c756dc854714657703839244b713f"
 
 */
--- UNIDADE_3/API/hashSenha/modules/loginUser.js
+++ UNIDADE_3/API/hashSenha/modules/loginUser.js
@@ -1,40 +1,32 @@
-import MUser from '../models/schemaUser.js';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import MUser from "../models/schemaUser.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 export async function loginUser(req, res) {
-const authHeader = req.headers['authorization'];
-const token = authHeader && authHeader.split(' ')[1];
-
-if (!token) {
-  return res.status(403).json({ error: 'Token não fornecido.' });
-}
-
   try {
     const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.status(400).json({ error: 'Email e senha são obrigatórios.' });
+      return res.status(400).json({ error: "Email e senha são obrigatórios." });
     }
 
     const user = await MUser.findOne({ email });
 
-    const validPassword = user ? await bcrypt.compare(password, user.password) : false;
+    const validPassword = user
+      ? await bcrypt.compare(password, user.password)
+      : false;
 
     if (!user || !validPassword) {
-      return res.status(401).json({ error: 'Email ou senha inválidos.' });
+      return res.status(401).json({ error: "Email ou senha inválidos." });
     }
 
-    const token = jwt.sign(
-      { userId: user._id },
-      process.env.JWT_SECRET,
-      { expiresIn: '1h' }
-    );
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
 
-    res.json({ message: 'Login realizado com sucesso!', token });
-
+    res.json({ message: "Login realizado com sucesso!", token });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Ocorreu um erro interno no servidor.' });
+    res.status(500).json({ error: "Ocorreu um erro interno no servidor." });
   }
 }
