eee7f003cfd404930867d0c4a64010ebc21bdcbd
Lucas Sassi <sassi.lucas17@gmail.com>
2025-11-19 11:22:09 -0300

    testando os baguio aqui

---
+++ UNIDADE_JEST/boilerplate-lions-dev/src/__tests__/integration/repository/user.find-by-id.int.test.js
@@ -0,0 +1,21 @@
+import userModel from "../../../models/user.model.js";
+import userRepository from "../../../repositories/user.repository.js";
+
+describe("when we try to find a user by a valid id", () => {
+  it("returns the correct user", async () => {
+    const userData = {
+      name: "Find By ID User",
+      email: `findbyid-${Date.now()}@example.com`,
+      password: "strongpasswordforfindbyid123",
+    };
+
+    const createdUser = await userModel.create(userData);
+
+    const foundUser = await userRepository.findById(createdUser._id);
+
+    expect(foundUser).toBeDefined();
+
+    expect(foundUser._id.toString()).toBe(createdUser._id.toString());
+    expect(foundUser.email).toBe(userData.email.toLowerCase());
+  });
+});
